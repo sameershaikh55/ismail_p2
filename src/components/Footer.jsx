1d84b557c0f2dd2bce56a9b298b69f209afa85d0
sameer-shaikh <sameermubeen4@gmail.com>
2021-04-29 01:52:01 +0500

    done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,33 +9,38 @@ import message from "../assets/message.svg";
 
 const Footer = () => {
 	return (
-		<div className="footer_container">
-			<div className="d-flex justify-content-center align-items-center">
-				<div className="imgContainer pr-5 py-2">
-					<img src={logoDark} alt="logo" />
+		<>
+			<div className="footer_container">
+				<div className="d-flex justify-content-center align-items-center">
+					<div className="imgContainer pr-5 py-2">
+						<img src={logoDark} alt="logo" />
+					</div>
+					<div className="ml-5 mt-n1 py-2 imgContainer2">
+						<img src={fb} alt="facebook" />
+						<img src={insta} alt="instagram" className="ml-3" />
+						<img src={linkdin} alt="linkdin" className="ml-3" />
+						<img src={skype} alt="skype" className="ml-3" />
+					</div>
 				</div>
-				<div className="ml-5 mt-n1 py-2 imgContainer2">
-					<img src={fb} alt="facebook" />
-					<img src={insta} alt="instagram" className="ml-3" />
-					<img src={linkdin} alt="linkdin" className="ml-3" />
-					<img src={skype} alt="skype" className="ml-3" />
+				<div className="d-flex justify-content-center mt-4 mx-auto align-items-center">
+					<div className="d-flex align-items-center mr-4">
+						<img src={message} alt="message" />
+						<a className="ml-2 mt-1 text-dark h5" href="info@gmail.com">
+							info@gmail.com
+						</a>
+					</div>
+					<div className="d-flex align-items-center ml-4">
+						<img src={phone} alt="phone" />
+						<a className="ml-2 mt-1 text-dark h5" href="877596621">
+							877596621
+						</a>
+					</div>
 				</div>
 			</div>
-			<div className="d-flex justify-content-around w-50 mt-4 mx-auto align-items-center">
-				<div className="d-flex align-items-center">
-					<img src={message} alt="message" />
-					<a className="ml-2 text-dark h5" href="info@gmail.com">
-						info@gmail.com
-					</a>
-				</div>
-				<div className="d-flex align-items-center ml-n5">
-					<img src={phone} alt="phone" />
-					<a className="ml-2 text-dark h5" href="877596621">
-						877596621
-					</a>
-				</div>
+			<div className="copyWriteLine text-center py-5 text-white">
+				© 2020Lozpdata . All Rights Reserved. Terms Of Use. Privacy Policy.
 			</div>
-		</div>
+		</>
 	);
 };
 
