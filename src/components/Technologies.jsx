b50ea14a36b4d5bc90e745c1b61091d9fa496681
sameer-shaikh <sameermubeen4@gmail.com>
2021-05-06 01:50:13 +0500

    bug fixed

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -11,37 +11,39 @@ const Technologies = () => {
 					<span className="themeText">TECHNOLOGIES</span> WE WORK WITH
 				</h1>
 			</div>
-			<div className="row">
-				<div className="col-12 col-sm-10 col-md-8 mx-auto">
-					<Tabs>
-						<TabList>
-							<Tab>Mobile</Tab>
-							<Tab>Front End</Tab>
-							<Tab>Database</Tab>
-							<Tab>Back end</Tab>
-							<Tab>CMS</Tab>
-							<Tab>Infra and Devops</Tab>
-						</TabList>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-12 col-sm-10 col-md-8 mx-auto">
+						<Tabs>
+							<TabList>
+								<Tab>Mobile</Tab>
+								<Tab>Front End</Tab>
+								<Tab>Database</Tab>
+								<Tab>Back end</Tab>
+								<Tab>CMS</Tab>
+								<Tab>Infra and Devops</Tab>
+							</TabList>
 
-						<TabPanel className="my-2">
-							<TechnologyBody />
-						</TabPanel>
-						<TabPanel>
-							<TechnologyBody fontEnd />
-						</TabPanel>
-						<TabPanel>
-							<TechnologyBody database />
-						</TabPanel>
-						<TabPanel>
-							<TechnologyBody backEnd />
-						</TabPanel>
-						<TabPanel>
-							<TechnologyBody cms />
-						</TabPanel>
-						<TabPanel>
-							<TechnologyBody infra />
-						</TabPanel>
-					</Tabs>
+							<TabPanel className="my-2">
+								<TechnologyBody />
+							</TabPanel>
+							<TabPanel>
+								<TechnologyBody fontEnd />
+							</TabPanel>
+							<TabPanel>
+								<TechnologyBody database />
+							</TabPanel>
+							<TabPanel>
+								<TechnologyBody backEnd />
+							</TabPanel>
+							<TabPanel>
+								<TechnologyBody cms />
+							</TabPanel>
+							<TabPanel>
+								<TechnologyBody infra />
+							</TabPanel>
+						</Tabs>
+					</div>
 				</div>
 			</div>
 		</div>
