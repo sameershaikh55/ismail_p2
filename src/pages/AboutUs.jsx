d229cd2cbe73f28347583b9828b7d05496a73166
sameer-shaikh <sameermubeen4@gmail.com>
2021-05-23 16:48:09 +0500

    d

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -23,7 +23,7 @@ const AboutUs = () => {
 			<div className="aboutUsHeader_container container-fluid d-flex flex-column justify-content-center align-items-center text-white">
 				<h1 className="mt-5 pt-5">ABOUT US</h1>
 				<h4 className="row text-center mt-3 mb-4">
-					<div className="col-12 col-sm-10 col-md-8 col-lg-6 m-auto headerPara">
+					<div className="col-12 col-sm-10 col-md-8 col-lg-6 m-auto headerPara h4">
 						When It Comes To Website Development, We Push The Limits And Set New
 						Standards By Creating Functionally Rich And Aesthetically Stunning
 						Websites. Best-In-Class UX And Compelling
